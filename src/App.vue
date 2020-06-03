d08e6bd006c55f09ebc4cb0cfde9e7762f7bbd7d
17576052526 <727863984@qq.com>
2020-06-03 11:05:03 +0800

    打包后字体图标不显示问题修复

--- src/App.vue
+++ src/App.vue
@@ -15,24 +15,5 @@ export default {
 
 <style>
 @import './../static/css/css.css';
-/*字体图标*/
-@font-face {
-  font-family: 'Glyphicons Halflings';
-  src: url('./../static/fonts/glyphicons-halflings-regular.eot');
-  src: url('./../static/fonts/glyphicons-halflings-regular.eot?#iefix') format('embedded-opentype'), url('./../static/fonts/glyphicons-halflings-regular.woff2') format('woff2'), url('./../static/fonts/glyphicons-halflings-regular.woff') format('woff'), url('./../static/fonts/glyphicons-halflings-regular.ttf') format('truetype'), url('./../static/fonts/glyphicons-halflings-regular.svg#glyphicons_halflingsregular') format('svg');
-}
-.glyphicon {
-    position: relative;
-    top: 1px;
-    display: inline-block;
-    font-family: 'Glyphicons Halflings';
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-}
-.glyphicon-home:before {content: "\e021";}
-.glyphicon-edit:before {content: "\e065";}
-.glyphicon-tree-deciduous:before {content: "\e200";}
+
 </style>
