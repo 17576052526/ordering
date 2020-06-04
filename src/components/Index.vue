2243838d60a4a0fa6b6a073928854cf824a99d21
17576052526 <727863984@qq.com>
2020-06-04 17:32:00 +0800

    aa

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="box">
         <BannerSlide/>
-        <div class="contact-box">
+        <div class="content-box">
             <div class="img-box">
                 <div><img src="http://img1.imgtn.bdimg.com/it/u=1212239151,1890480190&fm=26&gp=0.jpg" /></div>
                 <div><img src="http://img1.imgtn.bdimg.com/it/u=2030556663,777201328&fm=26&gp=0.jpg" /></div>
@@ -21,7 +21,7 @@ export default {
 
 <style scoped>
     .box{padding-bottom: 60px;}
-    .contact-box{padding:10px 10px;}
+    .content-box{padding:10px 10px;}
     .img-box{display: flex;margin-left: -5px;margin-right: -5px;}
     .img-box>div{margin:0px 5px;flex: 1;}
     .img-box img{width:100%;height:150px;display: block;}
